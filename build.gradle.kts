00c864de6a24a4badf27ac09127e6a4633a365aa
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-02-19 21:38:40 +0200

    removed dependencies from root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,6 @@ buildscript {
         google()
         jcenter()
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.6")
-        classpath(kotlin("gradle-plugin", version = "1.4.30"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
-        classpath("com.akaita.android:easylauncher:1.3.1")
-    }
 }
 
 allprojects {
