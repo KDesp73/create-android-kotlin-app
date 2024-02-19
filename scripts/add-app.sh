4fa963b266f7a6f5b87af39171e1d5bfc3931fbd
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-02-19 23:30:28 +0200

    scripts in scripts directory

---
+++ scripts/add-app.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+project_dir=$(cd "$(dirname "$0")" && pwd)
+
+
+if [ -z "$1" ]; then
+    printf "Enter name of apk to launch: "
+    read -r apk
+fi
+
+if [ -z "$2" ]; then
+    printf "Release or Debug [r/d]: "
+    read -r mode
+else
+    mode="$2"
+fi
+
+if [ "$mode" != 'r' ] && [ "$mode" != "release" ] && [ "$mode" != 'd' ] && [ "$mode" != "debug" ]; then
+    echo "Invalid build mode. Try 'release' or 'debug'"
+    exit 1
+fi
+
+if [ "$mode" == 'r' ]; then
+    mode="release"
+fi
+
+if [ "$mode" == 'd' ]; then
+    mode="debug"
+fi
+
+file="$project_dir/$apk/build/outputs/apk/$mode/$apk-$mode.apk"
+
+if [ ! -f "$file" ]; then
+    file="$project_dir/$apk/build/outputs/apk/$mode/$apk-$mode-unsigned.apk"
+fi
+
+cd ~/Android/Sdk/platform-tools || exit 1
+./adb install "$file" 
+exit 0
---
+++ scripts/launch-emulator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ~/Android/Sdk/emulator || exit 1
+
+echo "Available AVDs:"
+emulators=$(./emulator -list-avds)
+echo "$emulators"
+
+
+if [ "$(echo "$emulators" | wc -l)" -eq 1 ]; then
+    avd_name=$emulators
+else
+    echo "Select the AVD you want to launch:"
+    for ((i=0; i<${#emulators[@]}; i++)); do
+        echo "$i: ${emulators[i]}"
+    done
+    printf "Enter the number of the AVD you want to launch: "
+    read -r avd_index
+    avd_name=${emulators[avd_index]}
+fi
+
+./emulator -avd "$avd_name"
+
